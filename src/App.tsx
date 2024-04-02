b5dd84a6113ac4cda07cdb156a665f2ee06f1395
BuildTools <unconfigured@null.spigotmc.org>
2024-04-02 11:23:53 -0300

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+interface MeuBotaoProps{
+	texto: string
+}
+function MeuBotao(props: MeuBotaoProps){
+	return <button className="bg-orange-400 h-10">{props.texto}</button>
+}
 function App() {
-	return <h1>Hello Worlddd</h1>
+	return (
+		<div>
+			<MeuBotao texto="Clique aqui"/>
+		</div>
+	)
 }
 
 export default App
